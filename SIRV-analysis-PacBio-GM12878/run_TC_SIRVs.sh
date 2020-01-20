e10bdc29be3d07d3fdda04cfe57779d140741a15
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2020-01-19 22:32:43 -0800

    Script to run TC on SIRVs

---
+++ SIRV-analysis-PacBio-GM12878/run_TC_SIRVs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#$ -q sam128
+#$ -pe one-node-mpi 16
+#$ -R y
+#$ -N SIRV-TC-dry
+#$ -M dwyman@uci.edu
+#$ -m ea
+#$ -cwd
+#$ -j y
+
+module load dwyman/anaconda/3
+source activate mypython3.7.2
+
+mkdir -p TC_SIRV/Rep1
+mkdir -p TC_SIRV/Rep2
+mkdir -p TC_SIRV/both_reps
+
+cd TC_SIRV/Rep1
+time python /data/users/dwyman/TranscriptClean-2.0.2/TranscriptClean.py \
+    --sam ../../PacBio_Sequel2_GM12878_R1_SIRV_reads.sam \
+    --genome /share/crsp/lab/seyedam/share/PACBIO/genomes/SIRV/SIRV.fa \
+    --spliceJns /share/crsp/lab/seyedam/share/PACBIO/genomes/SIRV/SIRV_SJs.tsv  \
+    -t 16 \
+    --canonOnly \
+    --deleteTmp \
+    --outprefix TC
+time Rscript /data/users/dwyman/TranscriptClean-2.0.2/generate_report.R TC
+
+cd ../Rep2
+time python /data/users/dwyman/TranscriptClean-2.0.2/TranscriptClean.py \
+    --sam ../../PacBio_Sequel2_GM12878_R2_SIRV_reads.sam \
+    --genome /share/crsp/lab/seyedam/share/PACBIO/genomes/SIRV/SIRV.fa \
+    --spliceJns /share/crsp/lab/seyedam/share/PACBIO/genomes/SIRV/SIRV_SJs.tsv  \
+    -t 16 \
+    --canonOnly \
+    --deleteTmp \
+    --outprefix TC
+time Rscript /data/users/dwyman/TranscriptClean-2.0.2/generate_report.R TC
