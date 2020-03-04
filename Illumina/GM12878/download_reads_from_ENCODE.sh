b4a6fe4999c2f8f2ca2a0e0275f671f62a6d13d9
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2020-03-04 11:34:48 -0800

    Added Illumina files

---
+++ Illumina/GM12878/download_reads_from_ENCODE.sh
@@ -0,0 +1,16 @@
+# From https://www.encodeproject.org/experiments/ENCSR000AEH/
+
+## Replicate 1
+#wget https://www.encodeproject.org/files/ENCFF001RVT/@@download/ENCFF001RVT.fastq.gz
+#mv ENCFF001RVT.fastq.gz GM12878_rep1_illumina_1.fastq.gz
+
+#wget https://www.encodeproject.org/files/ENCFF001RVZ/@@download/ENCFF001RVZ.fastq.gz
+#mv ENCFF001RVZ.fastq.gz GM12878_rep1_illumina_2.fastq.gz
+
+## Replicate 2
+wget https://www.encodeproject.org/files/ENCFF001RVX/@@download/ENCFF001RVX.fastq.gz 
+mv ENCFF001RVX.fastq.gz GM12878_rep2_illumina_1.fastq.gz
+
+
+wget https://www.encodeproject.org/files/ENCFF001RWB/@@download/ENCFF001RWB.fastq.gz
+mv ENCFF001RWB.fastq.gz GM12878_rep2_illumina_2.fastq.gz
