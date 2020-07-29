8ecce3438aba86cd1cf45883c4456ddfeb0ab6d2
fairliereese <fairliek@comcast.net>
2020-07-29 13:28:28 -0700

    updated ends dir

--- end_analysis/README.md
+++ end_analysis/README.md
@@ -2,30 +2,42 @@
 
 For this analysis, we plotted a histogram of the distance from each reads' recorded start and end to its annotated start and end, for known transcripts. We attain the read starts and ends from the read_annot file (which is output from TALON), the annotated start and end from the annotation GTF (which is okay because we're only examining known transcripts that will be present in the annotation).
 
+Get the annotated starts/ends
+```bash
+gtf=/data/users/freese/mortazavi_lab/ref/gencode.v29/gencode.v29.annotation.gtf
+python get_annotated_TSSs_and_TESs.py \
+	--gtf $gtf \
+	--o gencode.v29.SIRV.ERCC.TSS_and_TES.tsv
+```
 For the PacBio data: 
 ```bash
 read_annot=/data/users/freese/talon_data/revisions_1-20/human_TALON/GM12878_talon_read_annot.tsv
-gtf=/data/users/freese/mortazavi_lab/ref/gencode.v29/gencode.v29.annotation.gtf
+annot_ends=gencode.v29.SIRV.ERCC.TSS_and_TES.tsv
 python plot_end_dists.py \
 	-f  $read_annot \
-	-g $gtf \
+	-a  $annot_ends \
 	--datasets 'PB_GM12878_R1,PB_GM12878_R2' \
 	-o 'pb'
 ```
 
+<img align="center" width="400" src="pb_tss_dists_hires.png">
 <img align="center" width="400" src="pb_tss_dists.png">
+<img align="center" width="400" src="pb_tes_dists_hires.png">
 <img align="center" width="400" src="pb_tes_dists.png">
 
 For the ONT data: 
 ```bash
 read_annot=/data/users/freese/talon_data/revisions_1-20/human_TALON/GM12878_talon_read_annot.tsv
-gtf=/data/users/freese/mortazavi_lab/ref/gencode.v29/gencode.v29.annotation.gtf
+annot_ends=gencode.v29.SIRV.ERCC.TSS_and_TES.tsv
 python plot_end_dists.py \
 	-f  $read_annot \
-	-g $gtf \
+	-a $annot_ends \
 	--datasets 'ONT_GM12878_R1,ONT_GM12878_R2' \
 	-o 'ont'
 ```
 
+<img align="center" width="400" src="ont_tss_dists_hires.png">
 <img align="center" width="400" src="ont_tss_dists.png">
+<img align="center" width="400" src="ont_tes_dists_hires.png">
 <img align="center" width="400" src="ont_tes_dists.png">
+
--- end_analysis/plot_end_dists.py
+++ end_analysis/plot_end_dists.py
@@ -8,8 +8,8 @@ def get_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', dest='infile', 
 		help='Read annot file')
-	parser.add_argument('-g', dest='gtf',
-		help='Annotation GTF')
+	parser.add_argument('-a', dest='annot_ends',
+		help='Ends from the reference annotation')
 	parser.add_argument('--datasets', dest='datasets',
 		help='CSV list of datasets to analyze', default=None)
 	parser.add_argument('-o', dest='oprefix',
@@ -17,12 +17,74 @@ def get_args():
 	args = parser.parse_args()
 	return args
 
+def plot_hires_bins(df, end_type, oprefix):
+	diff_col = '{}_diff'.format(end_type)
+	hires_bin_col = '{}_hires_bin'.format(end_type)
+	perc_col = 'perc_{}_hires'.format(end_type)
+
+	hires_total = len(df.loc[(df[diff_col] > -500)&(df[diff_col] <= 500)].index)
+	hires_bins = [i for i in range(-500,0,50)]
+	hires_bins += [-1, 0, 1]
+	hires_bins += [i for i in range(50,550,50)]
+
+	df[hires_bin_col] = pd.cut(df[diff_col], bins=hires_bins)
+	hires_df = df[[hires_bin_col, diff_col]].groupby(hires_bin_col).count()
+	hires_df.rename({diff_col: 'counts'}, axis=1, inplace=True)
+	hires_df.reset_index(inplace=True)
+	hires_df[perc_col] = (hires_df.counts/hires_total)*100
+	hires_df.counts.fillna(0, inplace=True)
+	hires_df[perc_col].fillna(0, inplace=True)
+
+	ax = sns.barplot(x=hires_bin_col, y=perc_col,
+			data=hires_df, color='#009E73', saturation=1)
+	ax.set(xlabel='Distance from annotated {} (bp)'.format(end_type.upper()))
+	ax.set(ylabel='Percentage of Known Reads within 500 bp')
+	ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+	ax.set(ylim=(0,100))
+	fname = '{}_{}_dists_hires.png'.format(oprefix, end_type)
+	print('Saving plot {}'.format(fname))
+	plt.savefig(fname,
+		bbox_inches='tight')
+	plt.clf()
+
+def plot_all_bins(df, end_type, oprefix):
+	diff_col = '{}_diff'.format(end_type)
+	bin_col = '{}_bin'.format(end_type)
+	perc_col = 'perc_{}'.format(end_type)
+
+	total = len(df.index)
+	bins = [i for i in range(-500,0,50)]
+	bins += [-1, 0, 1]
+	bins += [i for i in range(50,550,50)]
+	bins += [df[diff_col].max()]
+	bins = [df[diff_col].min()] + bins
+
+	df[bin_col] = pd.cut(df[diff_col], bins=bins)
+	bin_df = df[[bin_col, diff_col]].groupby(bin_col).count()
+	bin_df.rename({diff_col: 'counts'}, axis=1, inplace=True)
+	bin_df.reset_index(inplace=True)
+	bin_df[perc_col] = (bin_df.counts/total)*100
+	bin_df.counts.fillna(0, inplace=True)
+	bin_df[perc_col].fillna(0, inplace=True)
+
+	ax = sns.barplot(x=bin_col, y=perc_col,
+			data=bin_df, color='#009E73', saturation=1)
+	ax.set(xlabel='Distance from annotated {} (bp)'.format(end_type.upper()))
+	ax.set(ylabel='Percentage of Known Reads')
+	ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+	ax.set(ylim=(0,100))
+	fname = '{}_{}_dists.png'.format(oprefix, end_type)
+	print('Saving plot {}'.format(fname))
+	plt.savefig(fname,
+		bbox_inches='tight')
+	plt.clf()
+
 def main():
 	args = get_args()
 	infile = args.infile
 	datasets = args.datasets
 	oprefix = args.oprefix
-	gtf = args.gtf
+	annot_ends = args.annot_ends
 
 	df = pd.read_csv(infile, sep='\t')
 
@@ -34,57 +96,33 @@ def main():
 	# only use Known transcripts
 	df = df.loc[df.transcript_novelty == 'Known']
 
-	# remove SIRVs and ERCCs
+	# remove SIRVs and ERCCs and chrM
 	df = df.loc[~df.chrom.str.contains('SIRV')]
 	df = df.loc[~df.chrom.str.contains('ERCC')]
+	df = df.loc[df.chrom != 'chrM']
 
 
 	# pair it down only to the columns we need
 	df = df[['read_name', 'read_start', 'read_end', 'annot_transcript_id', 'strand']]
-	df.rename({'annot_transcript_id':'transcript_id'}, axis=1, inplace=True)
 
-	# read in gtf 
-	gtf_df = pd.read_csv(gtf, sep='\t', comment='#',
-		names=['feature_type', 'annot_start', 'annot_end', 'fields'],
-		usecols=[2,3,4,8])
-	gtf_df = gtf_df.loc[gtf_df.feature_type == 'transcript']
-	gtf_df['transcript_id'] = gtf_df.fields.str.extract(r'transcript_id "([A-z]+[0-9.]+)', expand=False)
-	# gtf_df[['beep', 'bop']] = gtf_df.fields.str.split(pat='transcript_id "', expand=True, n=1)
-	# gtf_df[['transcript_id', 'plop']] = gtf_df.bop.str.split(pat='";', expand=True, n=1)
-	gtf_df.drop(['fields', 'feature_type'], axis=1, inplace=True)
+	# read in annotation ends 
+	annot_df = pd.read_csv(annot_ends, sep='\t')
 
-	# merge gtf with the read annot file 
-	df = df.merge(gtf_df, how='left', on='transcript_id')
+	# Merge together reads with GENCODE start data
+	df = pd.merge(df, annot_df, on='annot_transcript_id', 
+					how='left')
 
-	# calculate distance between annotated and sequenced ends
-	df['tss_diff'] = np.nan
-	df['tes_diff'] = np.nan
-
-	# forward strand
-	df.loc[df.strand == '+', 'tss_diff'] = df['read_start'] - df['annot_start']
-	df.loc[df.strand == '+', 'tes_diff'] = df['read_end'] - df['annot_end']
-
-	# reverse strand 
-	df.loc[df.strand == '-', 'tss_diff'] = df['annot_start'] - df['read_start']
-	df.loc[df.strand == '-', 'tes_diff'] = df['annot_end'] - df['read_end']
+	# Compute TSS/TES distances
+	# Negative distance is upstream, positive is downstream
+	df['tss_diff'] = df.read_start - df.TSS_pos
+	df['tes_diff'] = df.read_end - df.TES_pos
+	df.loc[df['strand']=='-', ['tss_diff', 'tes_diff']] *= -1
 
 	# plot de plot
-	bins = [i for i in range(-800,801)]
-	ax = sns.distplot(df.tss_diff, kde=False, bins=bins)
-	ax.set(xlabel='Distance from annotated TSS (bp)')
-	ax.set(ylabel='Number of reads')
-	ax.set(xlim=(-800,800))
-	ax.set(ylim=(0,75000))
-	plt.savefig('{}_tss_dists.png'.format(oprefix))
-	plt.clf()
-
-	ax = sns.distplot(df.tes_diff, kde=False, bins=bins)
-	ax.set(xlabel='Distance from annotated TES (bp)')
-	ax.set(ylabel='Number of reads')
-	ax.set(xlim=(-800,800))
-	ax.set(ylim=(0,75000))
-	plt.savefig('{}_tes_dists.png'.format(oprefix))
-	plt.clf()
+	plot_hires_bins(df, 'tss', oprefix)
+	plot_hires_bins(df, 'tes', oprefix)
+	plot_all_bins(df, 'tss', oprefix)
+	plot_all_bins(df, 'tes', oprefix)
 
 if __name__ == '__main__':
 	main()
