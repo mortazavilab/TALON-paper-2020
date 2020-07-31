ded284a3f654d18c036044a553d375dd8df4843b
fairliereese <fairliek@comcast.net>
2020-07-31 14:35:41 -0700

    added stuff

--- TSS_and_TES/README.md
+++ TSS_and_TES/README.md
@@ -1,38 +1,55 @@
 # Plot distance from the start and end sites assoc with the intron chain of each known transcript
 
 1. Create a file listing the TSS and TES position for each annotated transcript:
-```
+```bash
 python get_annotated_TSSs_and_TESs.py \
     --gtf ../refs/GENCODE_v29/gencode.v29.SIRV.ERCC.annotation.gtf \
     --o gencode.v29.SIRV.ERCC.TSS_and_TES.tsv
 ```
 
-2. Plot distance of actual read start/emd from the annotated start/end associated with the annotated intron chain
-```
+2. Plot distance of actual read start/end from the annotated start/end associated with the annotated intron chain
+```bash
 python plot_TSS_and_TES_annot_dists.py \
     --f /share/crsp/lab/seyedam/share/TALON_paper_data/revisions_1-20/human_TALON/GM12878_talon_read_annot.tsv \
     --ref gencode.v29.SIRV.ERCC.TSS_and_TES.tsv \
     --datasets PB_GM12878_R1,PB_GM12878_R2 \
-    --xmax 800 --ymax 750000 --o GM12878_PacBio
+    --xmax 800 \
+    --ymax 750000 \
+    --o GM12878_PacBio
+```
+
+<img align="center" width="400" src="GM12878_PacBio_tss_dists_hires.png">
+<img align="center" width="400" src="GM12878_PacBio_TSS_dist_known.png">
+<img align="center" width="400" src="GM12878_PacBio_tes_dists_hires.png">
+<img align="center" width="400" src="GM12878_PacBio_TES_dist_known.png">
+
+We also get results about the number of known isoforms that have a read with a start that falls within 50 bp of the annotated TSS/TES
+
+```
+Found 17404 models with at least one read within 50 bp of the annotated TSS, which is 64.88% of known models
+Found 13876 models with at least one read within 50 bp of the annotated TES, which is 51.73% of known models
+Found 9457 models with at least one read within 50 bp of the annotated TSS and with at least one read within 50 bp of the annotated TES, which is 35.26% of known models
 ```
 
 # Plot distance of each read start (known transcripts only in PacBio) from closest CAGE peak
 1. Make a bed file of start sites
-```
+```bash
 python make_read_start_bed_file.py \
     --f /share/crsp/lab/seyedam/share/TALON_paper_data/revisions_1-20/human_TALON/GM12878_talon_read_annot.tsv \
     --datasets PB_GM12878_R1,PB_GM12878_R2 \
     --o GM12878_PacBio
 ```
+
 2. Run bedtools closest utility on start site file and CAGE peak file
-```
+```bash
 bedtools closest -a <(bedtools sort -i GM12878_PacBio_known_read_starts.bed) \
                  -b <(bedtools sort -i ../CAGE_data/FANTOM5/hg38_CAGE.bed) \
                  -t first \
                  -s -D a -nonamecheck > GM12878_PacBio_bedtools_closest.tsv
 ```
+
 3. Plot histogram
-```
+```bash
 python plot_closest_CAGE.py \
     --f GM12878_PacBio_bedtools_closest.tsv \
     --xmax 800 --ymax 3500000 --o GM12878_PacBio    
--- TSS_and_TES/plot_TSS_and_TES_annot_dists.py
+++ TSS_and_TES/plot_TSS_and_TES_annot_dists.py
@@ -31,7 +31,7 @@ def plot_histogram(data, xvar, label, xmax, ymax, fname):
     x = pd.Series(data[xvar], name=label)
     plt.xlim(-1*xmax,xmax)
     plt.ylim(0,ymax)
-    ax = sns.distplot(x, kde = False, color = "dodgerblue",
+    ax = sns.distplot(x, kde = False, color = '#009E73',
                   bins = np.arange(min(x), max(x), 5))
 
     med = round(np.median(x), 1)
@@ -40,16 +40,84 @@ def plot_histogram(data, xvar, label, xmax, ymax, fname):
     plt.axvline(med, linestyle = '--', color = 'lightgrey')
 
     ax.text(med + 50, ymax*7/8, "Median: " + str(med) + " bp", **style)
+    ax.set(ylabel='Number of reads')
 
     plt.tight_layout()
     plt.savefig(fname, dpi = 600, bbox_inches='tight')
     plt.close()
 
+def plot_hires_bins(df, end_type, oprefix):
+    # diff_col = '{}_diff'.format(end_type)
+    diff_col = '{}_dist'.format(end_type.upper())
+    hires_bin_col = '{}_hires_bin'.format(end_type)
+    perc_col = 'perc_{}_hires'.format(end_type)
+
+    hires_total = len(df.loc[(df[diff_col] > -500)&(df[diff_col] <= 500)].index)
+    hires_bins = [i for i in range(-500,0,50)]
+    hires_bins += [-1, 0, 1]
+    hires_bins += [i for i in range(50,550,50)]
+
+    df[hires_bin_col] = pd.cut(df[diff_col], bins=hires_bins)
+    hires_df = df[[hires_bin_col, diff_col]].groupby(hires_bin_col).count()
+    hires_df.rename({diff_col: 'counts'}, axis=1, inplace=True)
+    hires_df.reset_index(inplace=True)
+    hires_df[perc_col] = (hires_df.counts/hires_total)*100
+    hires_df.counts.fillna(0, inplace=True)
+    hires_df[perc_col].fillna(0, inplace=True)
+
+    ax = sns.barplot(x=hires_bin_col, y=perc_col,
+            data=hires_df, color='#009E73', saturation=1)
+    ax.set(xlabel='Distance from annotated {} (bp)'.format(end_type.upper()))
+    ax.set(ylabel='Percentage of Known Reads within 500 bp')
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+    ax.set(ylim=(0,100))
+    fname = '{}_{}_dists_hires.png'.format(oprefix, end_type)
+    print('Saving plot {}'.format(fname))
+    plt.savefig(fname,
+        bbox_inches='tight')
+    plt.clf()
+
+def calc_model_read_support(df, r):
+    n_models = len(df.annot_transcript_id.unique())
+
+    tss_models = df.loc[df.TSS_dist.abs() <= r,
+        'annot_transcript_id'].to_frame()
+    tss_models.drop_duplicates(inplace=True)
+    tes_models = df.loc[df.TES_dist.abs() <= r,
+        'annot_transcript_id'].to_frame()
+    tes_models.drop_duplicates(inplace=True)
+
+
+    both_models = tss_models.merge(tes_models, how='inner',
+        on='annot_transcript_id')
+
+    n_tss_models = len(tss_models.index)
+    n_tes_models = len(tes_models.index)
+    n_both_models = len(both_models.index)
+
+    beep = 'Found {} models with at least one read within {} bp '+\
+          'of the annotated TSS, which is {:.2f}% of known '+\
+          'models'
+    print(beep.format(n_tss_models, r, (n_tss_models/n_models)*100))
+
+    beep = 'Found {} models with at least one read within {} bp '+\
+          'of the annotated TES, which is {:.2f}% of known '+\
+          'models'
+    print(beep.format(n_tes_models, r, (n_tes_models/n_models)*100))
+
+    beep = 'Found {} models with at least one read within {} bp '+\
+          'of the annotated TSS and with at least one read within {} '+\
+          'bp of the annotated TES, which is {:.2f}% of known '+\
+          'models'
+    print(beep.format(n_both_models, r, r, (n_both_models/n_models)*100))
+
 def main():
     options = getOptions()
+    infile = options.infile
+    ref_sites = options.ref_sites
 
-    data = pd.read_csv(options.infile, sep = '\t', header = 0)
-    ref_sites = pd.read_csv(options.ref_sites, sep = '\t', header = 0)
+    data = pd.read_csv(infile, sep = '\t', header = 0)
+    ref_sites = pd.read_csv(ref_sites, sep = '\t', header = 0)
 
     # Limit to known transcripts
     data = data.loc[data.transcript_novelty == "Known"]
@@ -79,12 +147,25 @@ def main():
     data.loc[data['strand']=='-', ['TSS_dist', 'TES_dist']] *= -1
 
     # Plot
-    plot_histogram(data, "TSS_dist", "Distance from TSS annotated to intron chain (bp)", 
+    plot_histogram(data, "TSS_dist", "Distance from annotated TSS (bp)", 
                    options.xmax, options.ymax, 
                    options.outprefix + "_TSS_dist_known.png")
-    plot_histogram(data, "TES_dist", "Distance from TES annotated to intron chain (bp)",
+    plot_histogram(data, "TES_dist", "Distance from annotated TES (bp)",
                    options.xmax, options.ymax,
                    options.outprefix + "_TES_dist_known.png")
+
+    oprefix = options.outprefix
+    plot_hires_bins(data, 'tss', oprefix)
+    plot_hires_bins(data, 'tes', oprefix)
+
+    # now we want to see 
+    # 1. what percentage of known transcripts have a read that falls
+    # within 50 bp of the annotated TSS
+    # 2. what percentage of known transcripts have a read that falls
+    # within 50 bp of the annotated TES
+    # 3. what percentage of known transcripts have both? ( these can 
+    # be from different reads )
+    calc_model_read_support(data, 50)
 
 if __name__ == '__main__':
     main()
